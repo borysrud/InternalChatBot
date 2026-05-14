c4afbdc39dbec22e374337261d0db2ad7e9a0336
borysrud <borysrud@gmail.com>
2026-05-14 15:26:04 -0400

    Initial Commit

---
+++ InternalChatBot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InternalChatBot
+//
+//  Created by Borys Rud on 2026-05-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InternalChatBot/InternalChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternalChatBotApp.swift
+//  InternalChatBot
+//
+//  Created by Borys Rud on 2026-05-14.
+//
+
+import SwiftUI
+
+@main
+struct InternalChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
